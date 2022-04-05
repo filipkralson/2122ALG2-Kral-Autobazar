48f14447badedc49b33455eb98d99ed2d374e9d3
FilipKralson <filip.kral1@tul.cz>
2022-04-05 09:13:04 +0200

    start project

---
+++ Autobazar/src/Autobazar.java
@@ -0,0 +1,10 @@
+public class Autobazar {
+    
+    //pracuješ za majitele, vytvoříš si autobazar s danným počtem stání a počtem zaměstanců, nakoupíš do něj auta a zaměstnáš prodejce, 
+    //budeš mít nad auty celkovou správu (vypsání značek atd.),
+    //najímání zaměstanců s úrovněmi pro kvalitnější prodej, čím vyšší level, tím vyšší cena prodeje, náhodně budou chodit zákazníci,
+    //kteří budou chtít náhodná auta z bazaru, můžeš k němu vyslat jakéhokoliv zaměstnance, kterého chceš
+    //správa peněz autobazaru pro vyplácení platů a kupování dalších aut pro doplnění stání
+    //výplata bude probíhat po volitelném počtu prodejů
+    //na výběr databáze 50 aut pro koupi (použití BufferReader)
+}
---
+++ Autobazar/src/AutobazarApp.java
@@ -0,0 +1,6 @@
+public class AutobazarApp {
+    public static void main(String[] args) {
+        
+    }
+    
+}
